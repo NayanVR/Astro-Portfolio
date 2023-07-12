d8eb2ae45d02f94c4f47de784da73d376d6e6a9b
NayanVR <nvradadiya281@gmail.com>
2023-07-12 16:04:11 +0530

    hero section ready

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'dark': '#050505',
+				'dark-1': '#121212',
+				'dark-2': '#242424',
+				'light': '#F6F6F6',
+				'light-1': '#9A9A9A',
+				'primary': '#2667FF',
+				'primary-dark': '#3B28CC',
+				'secondary': '#FFC700',
+				'secondary-dark': '#F2A000',
+				'tertiary': '#4CC700',
+				'tertiary-dark': '#0C9B00',
+			},
+			fontFamily: {
+				'sans': ['JetBrains Mono', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
